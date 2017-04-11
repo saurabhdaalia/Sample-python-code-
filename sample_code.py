59bb59f866faddcde26cb1b0752bd6213de482ef
Saurabh <saurabhdalia@gmail.com>
2017-04-11 09:30:50 +0530

    Code finished

--- sample_code.py
+++ sample_code.py
@@ -1,5 +1,12 @@
-""" This is a sample python code
-    to show the working of the 
-	version control system """
+#This is a sample python code
+   # to show the working of the 
+	#version control system """
 	
-	
+# A simple program to add to numbers
+
+a = 1 #first number
+b = 2 # second number
+
+sum=a+b # adding
+
+print "The sum is ", sum 
